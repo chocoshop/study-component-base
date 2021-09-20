fe2365605cdc52dcf238ada649251d63a717d00d
chocoshop <15ec112j@rikkyo.ac.jp>
2021-09-20 18:42:26 +0900

    ディレクトリを整理

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function TestComponent() {
+    return (
+        <div>Hello from npm package</div>
+    );
+}
+
+export function Test2() {
+    return (
+        <div>Hello test2 from npm package</div>
+    );
+}
